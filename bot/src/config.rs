56aaacfbdab92e5ee0bfa8ddbf42f3844b3b1bf3
evvv_vvve <76193592+evvv-vvve@users.noreply.github.com>
2022-08-25 00:27:18 -0500

    documentation, config error handling

--- bot/src/config.rs
+++ bot/src/config.rs
@@ -1,14 +1,42 @@
+/// Errors that can occur with a config file
+#[derive(thiserror::Error, Debug)]
+pub enum ConfigError {
+    /// Returned when a config file could
+    /// not be read from file
+    #[error("Could not read config file")]
+    ConfigReadError,
+
+    /// Returned when a config file could
+    /// not be parsed
+    #[error("Could not parse config file")]
+    ConfigParseError
+}
+
+/// A collection of configuration values
+/// for Yukimi
 #[derive(serde::Serialize, serde::Deserialize)]
 pub struct Config {
+    /// Login token for Discord
     token: String,
 }
 
 impl Config {
-    pub fn new(token: &str) -> Self {
-        Self {
-            token: String::from(token)
+    /// Attempts to read a file into a string, then parse it
+    /// using toml.
+    /// 
+    /// Returns a `ConfigError` if either step fails.
+    pub fn from_file(path: &str) -> Result<Self, ConfigError> {
+        if let Ok(contents) = std::fs::read_to_string(path) {
+            if let Ok(config) = toml::from_str::<Self>(contents.as_str()) {
+                Ok(config)
+            } else {
+                Err(ConfigError::ConfigParseError)
+            }
+        } else {
+            Err(ConfigError::ConfigReadError)
         }
     }
 
+    /// Returns the value of `token`
     pub fn get_token(&self) -> String { self.token.clone() }
 }
--- bot/src/main.rs
+++ bot/src/main.rs
@@ -1,5 +1,4 @@
-use std::fs;
-
+use config::Config;
 use serenity::async_trait;
 use serenity::prelude::*;
 use serenity::model::channel::Message;
@@ -26,24 +25,26 @@ impl EventHandler for Handler { }
 
 #[tokio::main]
 async fn main() {
-    let framework = StandardFramework::new()
-      .configure(|conf| conf.prefix("y!"))
-      .group(&GENERAL_GROUP);
+  let framework = StandardFramework::new()
+    .configure(|conf| conf.prefix("y!"))
+    .group(&GENERAL_GROUP);
     
-    let config_contents = fs::read_to_string("config.toml")
-       .expect("Could not read config file");
+  match Config::from_file("config.toml") {
+    Ok(config) => {
+      let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
 
-    let config: config::Config = toml::from_str(config_contents.as_str()).expect("Could not parse config file");
-    
-    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
+      let mut client = Client::builder(config.get_token(), intents)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Couldn't create client!");
 
-    let mut client = Client::builder(config.get_token(), intents)
-      .event_handler(Handler)
-      .framework(framework)
-      .await
-      .expect("Couldn't create client!");
-
-    if let Err(err) = client.start().await {
-      println!("Error while running client: {err:?}")
+      if let Err(err) = client.start().await {
+        println!("Error while running client: {err:?}")
+      }
+    },
+    Err(config_error) => {
+      println!("An error occurred while loading config: {config_error:#}")
     }
+  }
 }
